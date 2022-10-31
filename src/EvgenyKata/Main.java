a070d207220b592e9ca7cc697e8f477952f38a4e
Evgeny Lukin <azzy28@gmail.com>
2022-10-31 09:48:49 +0300

    updated Kata3: - corrected wrong loop for Clearing 0

--- src/EvgenyKata/Main.java
+++ src/EvgenyKata/Main.java
@@ -2,14 +2,21 @@ package EvgenyKata;
 
 import KatasAll.Kata1;
 import KatasAll.Kata2;
+import KatasAll.Kata3;
 
 public class Main {
     public static void main(String[] args) {
 
         //Kata 1
         //System.out.println(Kata1.solution(20));
-        // Kata 2
+
+        //Kata 2
         //System.out.println(Kata2.hex(-20, 275, 125));
+
+        //Kata 3 - 525f4206b73515bffb000b21
+        //System.out.println(Kata3.add("63829983432984289347293874", "90938498237058927340892374089"));
+        System.out.println("Sum is: " + Kata3.add("106573748643738532845228228053", "00000533538555311536708234764171152"));
+
 
     }
 }
--- src/KatasAll/Kata3.java
+++ src/KatasAll/Kata3.java
@@ -1,4 +1,52 @@
 package KatasAll;
 
+//https://www.codewars.com/kata/525f4206b73515bffb000b21/java
+
+//import java.math.BigInteger;
+
+import com.sun.source.tree.WhileLoopTree;
+
 public class Kata3 {
+
+    public static String clear (String toClear) {
+        while ((int)(toClear.charAt(0) - '0') == 0) toClear = toClear.substring(1, toClear.length());
+        return toClear;
+    }
+
+    public static String add (String a, String b) {
+
+        //make sure first str is less
+        if (a.length() > b.length()) {
+            String new1 = b;
+            b = a;
+            a = new1;
+        }
+
+        String result = "";
+        a = clear(a);
+        b = clear(b);
+        int carry = 0;
+        int lengthA = a.length();
+        int lengthB = b.length();
+        int diff = lengthB - lengthA;
+
+        for (int i = lengthA-1; i >= 0; i-- ) {
+            int sum = (int)((a.charAt(i)) - '0') + (int)((b.charAt(i+diff)) - '0') + carry;
+            result += (char)(sum % 10 + '0');
+            carry = sum / 10;
+            //System.out.println((int)(a.charAt(i)) + "....." + (int)(b.charAt(i+diff)));
+            //System.out.println(sum);
+        }
+
+        for (int i = diff-1; i >= 0; i--) {
+            int sum = (int)((b.charAt(i) - '0')) + carry;
+            result += (char)(sum % 10 + '0');
+            carry = sum / 10;
+        }
+
+        if (carry > 0) result += (char)(carry + '0');
+
+        return new StringBuilder(result).reverse().toString();
+
+    }
 }
